228c2628e229caa438adca714e9018c1e3e72a8f
cyberwithjune <newswitcyber@gmail.com>
2024-04-23 21:50:04 -0400

    adding files to gcp terraform

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.25.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+project = "red-studio-419223"
+region = "us-central1"
+zone = "us-central1-a"
+credentials = "red-studio-419223-f2e31318e0ad.json"
+
+}
+
+resource "google_storage_bucket" "static-site1" {
+  name          = "osaka_oh_yea"
+  location      = "us-central1"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
